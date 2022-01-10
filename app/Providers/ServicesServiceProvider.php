c0aa6533f6cdec9c08f0b10c3fc636b27cbad842
Nuno França <alberto.oliveira.ext@dasa.com.br>
2022-01-10 07:21:13 -0300

    Iserts protect command

--- app/Providers/ServicesServiceProvider.php
+++ app/Providers/ServicesServiceProvider.php
@@ -2,10 +2,24 @@
 
 namespace App\Providers;
 
+use App\Commands\CreateBase;
+use App\Commands\CreatePattern;
+use App\Commands\CreateRepository;
+use App\Commands\CreateRepositoryInterface;
+use App\Commands\CreateService;
+use App\Commands\CreateServiceInterface;
 use Illuminate\Support\ServiceProvider;
 
 class ServicesServiceProvider extends ServiceProvider
 {
+    protected $commands = [
+        CreateBase::class,
+        CreatePattern::class,
+        CreateRepository::class,
+        CreateRepositoryInterface::class,
+        CreateService::class,
+        CreateServiceInterface::class
+    ];
     /**
      * Bootstrap any application services.
      *
